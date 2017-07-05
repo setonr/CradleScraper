728c5035a5bcd5ccdd43412c594b79757ddd2851
E. Seton Raynor <esetonraynor@gmail.com>
2017-07-05 18:39:00 -0400

    mongodb uri

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(express.static(path.join(__dirname + "/public")));
 
 var mongodbURI = "mongodb://heroku_wcsq2591:of7uheoiv7q5rkie7sc28qfvqj@ds139082.mlab.com:39082/heroku_wcsq2591";
 
-// mongoose.connect(mongodbURI);
-mongoose.connect("mongodb://localhost/articles_db");
+mongoose.connect(mongodbURI);
+// mongoose.connect("mongodb://localhost/articles_db");
 var db = mongoose.connection;
 
 db.on("error", function(error) {
